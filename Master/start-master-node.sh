97209950178458c1d6c00bf60d5af511e242b7ae
Alan Peng <peng.alan@gmail.com>
2017-04-19 20:51:56 -0400

    1st commit

---
+++ Master/start-master-node.sh
@@ -0,0 +1,1 @@
+docker run -itd --name=MySQL-Cluster-Master -e MYSQL_ROOT_PASSWORD=password -e MySQL_Role=Master -e REPLICATION_PASSWORD=88888888 -e REPLICATION_USER=repluser -e MasterIP=172.20.10.1 -e Slave1IP=172.20.10.2 -e Slave2IP=172.20.20.6 -e MySQLPort=3307 -p 3307:3306 -v /home/rancher/mysql:/var/lib/mysql mysql5.6-replication-docker:1.0
---
+++ Slave1/start-slave1-node.sh
@@ -0,0 +1,1 @@
+docker run -itd --name=MySQL-Cluster-Slave1 -e MYSQL_ROOT_PASSWORD=password -e MySQL_Role=Slave1 -e REPLICATION_PASSWORD=88888888 -e REPLICATION_USER=repluser -e MasterIP=172.20.10.1 -e Slave1IP=172.20.10.2 -e Slave2IP=172.20.20.6 -e MySQLPort=3307 -p 3307:3306 -v /home/rancher/mysql:/var/lib/mysql mysql5.6-replication-docker:1.0
